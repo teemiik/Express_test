650af36678d0b27c67a508e89720ee8595857580
Artem Bolgov <teemiik@mail.ru>
2017-02-13 22:03:06 +0300

    ScreenTest and Dialog

---
+++ app/src/main/java/com/example/tanat/express_test/DialogScreen.java
@@ -0,0 +1,30 @@
+package com.example.tanat.express_test;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by tanat on 13.02.2017.
+ */
+
+public class DialogScreen extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder adb = new AlertDialog.Builder(getActivity())
+                .setTitle("Title!").setPositiveButton(R.string.b_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ((ScreenTestActivity) getActivity()).okClicked();
+                    }
+                })
+                .setNegativeButton(R.string.b_no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ((ScreenTestActivity) getActivity()).cancelClicked();
+                    }
+                })
+                .setMessage(R.string.vibor);
+        return adb.create();
+    }
+}
--- app/src/main/java/com/example/tanat/express_test/ScreenTestActivity.java
+++ app/src/main/java/com/example/tanat/express_test/ScreenTestActivity.java
@@ -1,43 +1,19 @@
 package com.example.tanat.express_test;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.GradientDrawable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
-import static android.app.PendingIntent.getActivity;
-import static com.example.tanat.express_test.R.color.notification_material_background_media_default_color;
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class ScreenTestActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-    TextView fullscreen_content;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
+public class ScreenTestActivity extends AppCompatActivity implements View.OnClickListener{
 
     /**
      * Some older devices needs a small delay between UI widget updates
@@ -46,6 +22,10 @@ public class ScreenTestActivity extends AppCompatActivity {
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
+    private final String DIALOG = "dialog";
+    DialogFragment dialogScreen;
+
+
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
@@ -82,24 +62,12 @@ public class ScreenTestActivity extends AppCompatActivity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        dialogScreen = new DialogScreen();
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_screen_test);
@@ -107,24 +75,7 @@ public class ScreenTestActivity extends AppCompatActivity {
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ColorDrawable vColor = (ColorDrawable) v.getBackground();
-                if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.red)) {
-                    v.setBackgroundResource(R.color.green);
-                } else if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.green)) {
-                    v.setBackgroundResource(R.color.blue);
-                } else if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.blue)) {
-                    v.setBackgroundResource(R.color.gray);
-                } else if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.gray)) {
-                    v.setBackgroundResource(R.color.black);
-                }
-                else {
-                    show(v);
-                }
-            }
-        });
+        mContentView.setOnClickListener(this);
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
@@ -138,7 +89,7 @@ public class ScreenTestActivity extends AppCompatActivity {
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
-        delayedHide(100);
+        delayedHide(1);
     }
 
 
@@ -155,21 +106,6 @@ public class ScreenTestActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    @SuppressLint("InlinedApi")
-    private void show(View v) {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        fullscreen_content = (TextView) findViewById(R.id.fullscreen_content);
-        v.setBackgroundResource(R.color.white);
-        fullscreen_content.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
-        fullscreen_content.setText(R.string.vibor);
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
     /**
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
@@ -179,4 +115,29 @@ public class ScreenTestActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
+    @Override
+    public void onClick(View v) {
+        ColorDrawable vColor = (ColorDrawable) v.getBackground();
+        if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.red)) {
+            v.setBackgroundResource(R.color.green);
+        } else if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.green)) {
+            v.setBackgroundResource(R.color.blue);
+        } else if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.blue)) {
+            v.setBackgroundResource(R.color.gray);
+        } else if (vColor.getColor() == ContextCompat.getColor(getApplicationContext(), R.color.gray)) {
+            v.setBackgroundResource(R.color.black);
+        }
+        else {
+            dialogScreen.show(getSupportFragmentManager(), DIALOG);
+        }
+
+    }
+
+    public void okClicked() {
+        boolean i = true;
+    }
+
+    public void cancelClicked() {
+        boolean i = false;
+    }
 }
