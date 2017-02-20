ddce5a2a8150fedaa875cba61e1837cdc2b733d8
GreatTravis <givdrag@gmail.com>
2017-02-20 20:42:26 +0300

    Wf-fi, bluetooth

--- app/src/main/java/com/example/tanat/express_test/InterfaceWBG.java
+++ app/src/main/java/com/example/tanat/express_test/InterfaceWBG.java
@@ -1,34 +1,91 @@
 package com.example.tanat.express_test;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
-
+import android.telephony.CellSignalStrength;
+import android.os.Parcelable;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
+import android.telephony.CarrierConfigManager;
 
 public class InterfaceWBG extends AppCompatActivity {
 
     WifiManager wifiManager;
-    TextView t_wifi, t_bluetoth, t_glonass;
+    WifiInfo wifiInfo;
+    TextView t_wifi, t_bluetoth, tvGPS,tvDBM,tvNetworkType;
     BluetoothAdapter bluetooth;
-    int signal_s;
+    int signal_s, signal_a = 0;
+    String b_address;
+    TelephonyManager mTelephonyManager;
+    CarrierConfigManager mTelephonyManager2;
+    MyPhoneStateListener mPhoneStatelistener,mPhoneStatelistener2;
+    int mSignalStrength = 0;
+
+    private LocationManager locationManager;
+    StringBuilder sbGPS = new StringBuilder();
+    StringBuilder sbNet = new StringBuilder();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface_wbg);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        tvDBM = (TextView) findViewById(R.id.tvDBM);
+        tvNetworkType = (TextView) findViewById(R.id.tvNetworkType);
+
+
+        mPhoneStatelistener = new MyPhoneStateListener();
+        mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        mTelephonyManager.listen(mPhoneStatelistener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+
+        //mPhoneStatelistener2 = new MyPhoneStateListener();
+        mTelephonyManager2 = (CarrierConfigManager)  getSystemService(Context.CARRIER_CONFIG_SERVICE);
+       // mTelephonyManager2.listen(mPhoneStatelistener, PhoneStateListener.);
+
+
+
+
+
+
+
+
+        //проверка местоположения
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                InterfaceWBG.this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+                InterfaceWBG.this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            }
+        }
 
         bluetooth= BluetoothAdapter.getDefaultAdapter();
 
         t_wifi = (TextView) findViewById(R.id.t_wi_fi);
         t_bluetoth = (TextView) findViewById(R.id.t_bluetoth);
-        t_glonass = (TextView) findViewById(R.id.t_glonass);
+        tvGPS = (TextView) findViewById(R.id.tvGPS);
 
         //проверка включения wi-fi
         try {
@@ -38,21 +95,26 @@ public class InterfaceWBG extends AppCompatActivity {
                 wifiManager.setWifiEnabled(true);
             }
 
-            signal_s = this.getWifiSignalState();
-
+            wifiInfo = wifiManager.getConnectionInfo();
 
             //проверка наличия сетей wi-fi
-            if (wifiManager.startScan()) {
-                if (signal_s != 0) {
-                    t_wifi.setText(String.valueOf(signal_s));
-                    wifiManager.setWifiEnabled(false);
+            wifiManager.startScan();
+            List<ScanResult> results = wifiManager.getScanResults();
+            for (ScanResult result : results) {
+                if(result != null) {
+                    signal_a = WifiManager.calculateSignalLevel(result.level, 5);
+                    if (signal_a > signal_s) {
+                        // самый лучший сигнал
+                        signal_s = signal_a;
+                    }
                 }
-            } else {
-                t_wifi.setText("Сети wi-fi не найдены");
-                wifiManager.setWifiEnabled(false);
             }
+
+            t_wifi.setText(R.string.wifi_on);
+            wifiManager.setWifiEnabled(false);
+
         } catch (Exception e) {
-            t_wifi.setText("Wifi не работает");
+            t_wifi.setText(R.string.wifi_off);
         }
 
         //проверка налачия модуля bluetooth
@@ -60,40 +122,165 @@ public class InterfaceWBG extends AppCompatActivity {
             //провекра включенности
             if (!bluetooth.isEnabled()) {
                 if (bluetooth.enable()) {
-                    t_bluetoth.setText("Bluetooth работает");
+                    t_bluetoth.setText(R.string.bluet_on);
+                    //аппаратный адрес
+                    b_address = bluetooth.getAddress();
                     bluetooth.disable();
                 } else {
-                    t_bluetoth.setText("Bluetooth не работает");
+                    t_bluetoth.setText(R.string.bluet_off);
                 }
             }
         }
-
     }
 
-    public int getWifiSignalState() {
-        int signalStrength = 5;
-        List<ScanResult> results = wifiManager.getScanResults();
-        if (results != null) {
-            for (ScanResult result : results) {
-                if (result != null && wifiManager != null && wifiManager.getConnectionInfo() != null && result.BSSID != null
-                        && result.BSSID.equals(wifiManager.getConnectionInfo().getBSSID())) {
-                    int level = 0;
-                    level = wifiManager.calculateSignalLevel(wifiManager.getConnectionInfo().getRssi(), result.level);
-                    if (level != 0 && result.level != 0) {
-                        int difference = level * 100 / result.level;
-                        if (difference >= 100)
-                            signalStrength = 4;
-                        else if (difference >= 75)
-                            signalStrength = 3;
-                        else if (difference >= 50)
-                            signalStrength = 2;
-                        else if (difference >= 25)
-                            signalStrength = 1;
-                    }
-                }
-            }
-            signalStrength = results.size();
+      // данный метод выводит координаты в зависимости от постовщика (GPS или Моб сеть.)
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            tvGPS.setText(formatLocation(location)+ " GPS");
+        } else if (location.getProvider().equals(
+                LocationManager.NETWORK_PROVIDER)) {
+            tvGPS.setText(formatLocation(location)+ " NET");
+
         }
-        return signalStrength;
     }
+
+    // формат выходных данных (для красоты и нормального чтения)
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT",
+                location.getLatitude(), location.getLongitude(), new Date(
+                        location.getTime()));
+    }
+
+    //газ
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000 * 10, 10, locationListener);
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, locationListener);
+        //checkEnabled();
+    }
+    //тормоз
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
+
+
+    //слушатель координат с 3 состояниями по сути ебула полнейшая, потом исправлю
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+           // checkEnabled();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            //checkEnabled();
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+
+
+        //метод выводит вкл и выкл поставщика координат
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                //tvGPS.setText(" Status: " + String.valueOf(status));
+            } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                //tvGPS.setText("Status: " + String.valueOf(status));
+            }
+        }
+    };
+
+
+   /* private void checkEnabled() {
+        tvGPS.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER));
+        tvGPS.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER));
+    }*/
+
+    //в душе не чаю как это работает, но работает и это прекрасно. Не трогать. Warning! I don't know
+    // how is it work.
+    class MyPhoneStateListener extends PhoneStateListener {
+
+        @Override
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+            super.onSignalStrengthsChanged(signalStrength);
+            mSignalStrength = signalStrength.getGsmSignalStrength();
+            //mSignalStrength = signalStrength.getCdmaDbm();
+            mSignalStrength = (2 * mSignalStrength) - 113; // -> dBm
+
+
+            tvDBM.setText("Stats " +mSignalStrength);
+
+
+
+            int networkType = mTelephonyManager.getNetworkType();
+            String suka = CarrierConfigManager.KEY_CARRIER_VOLTE_TTY_SUPPORTED_BOOL;
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN:
+                    tvNetworkType.setText("2g only");
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                case TelephonyManager.NETWORK_TYPE_EHRPD:
+                case TelephonyManager.NETWORK_TYPE_HSPAP:
+                    tvNetworkType.setText("2g/3g is Supported");
+                case TelephonyManager.NETWORK_TYPE_LTE:
+                    tvNetworkType.setText("2g/3g/4g LTE is Supported");
+
+                       tvNetworkType.setText(suka);
+
+                //default:
+                  //  tvNetworkType.setText("Not found");
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
