455824a020972ff8b55ae13f0bf65d36c958429b
Artem Bolgov <teemiik@mail.ru>
2017-03-25 00:02:14 +0300

    Дозалив

--- app/src/main/java/com/example/tanat/express_test/GeneralActivity.java
+++ app/src/main/java/com/example/tanat/express_test/GeneralActivity.java
@@ -2,14 +2,22 @@ package com.example.tanat.express_test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Set;
 
 public class GeneralActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button vibor_express;
+    Button b_report;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,9 @@ public class GeneralActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_general);
 
         vibor_express = (Button) findViewById(R.id.vibor_express);
+        b_report = (Button) findViewById(R.id.b_report);
         vibor_express.setOnClickListener(this);
+        b_report.setOnClickListener(this);
 
     }
 
@@ -27,8 +37,36 @@ public class GeneralActivity extends AppCompatActivity implements View.OnClickLi
             Intent intent = new Intent(this, ScreenTestActivity.class);
             startActivity(intent);
         }
+        else if (v.getId() == R.id.b_report) {
+            Intent intent = new Intent(this, ReportActivity.class);
+            startActivity(intent);
+        }
     }
 
+
+    //функция записи отчета
+    public static void recordReport(HashMap report) {
+        String file_name = "report.txt";
+        String text;
+        Set<HashMap.Entry> set_values = report.entrySet();
+        try {
+            File folder = new File(Environment.getExternalStorageDirectory() +
+                    File.separator + "android" + File.separator + "data" + File.separator + "com.android.express_test");
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+            File file_report = new File(folder, file_name);
+            file_report.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file_report, true);
+            for (HashMap.Entry value : set_values) {
+                text = String.valueOf(value.getKey()) + " | " + String.valueOf(value.getValue() + "\n");
+                outputStream.write(text.getBytes());
+            }
+            outputStream.close();
+        } catch (Exception ex) {
+            Log.e("report" ,ex.getMessage());
+        }
+    }
 
 }
 
